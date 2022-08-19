580a05dbf914d866731d6e97d1babe97798ab7dd
tuesdaydude <ivanenko0607@gmail.com>
2022-08-19 22:35:51 +0300

    Requests Presentator Implementation

--- Source/BSTU/RequestsScheduler/Interactor/Presentators/RequestsPresentator.cs
+++ Source/BSTU/RequestsScheduler/Interactor/Presentators/RequestsPresentator.cs
@@ -5,11 +5,32 @@ namespace BSTU.RequestsScheduler.Interactor.Presentators
 {
     public class RequestsPresentator : IRequestsPresentator
     {
+        private readonly IRequestsInteractor _requestsInteractor;
+
         public RequestsPresentator(IRequestsInteractor requestsInteractor)
         {
-
+            _requestsInteractor = requestsInteractor;
         }
 
-        public IEnumerable<Queue<Request>> RequestQueues => throw new NotImplementedException();
+        public IEnumerable<Queue<Request>> RequestQueues
+        {
+            get
+            {
+                IEnumerable<Request> requests = _requestsInteractor.Requests;
+
+                var busStopNames = new HashSet<string>(requests.Select(request => request.SourceBusStopName));
+
+                var requestQueues = new List<Queue<Request>>();
+                
+                foreach (string busStopName in busStopNames)
+                {
+                    var requestQueue = new Queue<Request>(requests
+                        .Where(request => request.SourceBusStopName.Equals(busStopName, StringComparison.InvariantCultureIgnoreCase)));
+                    requestQueues.Add(requestQueue);
+                }
+
+                return requestQueues;
+            }
+        }
     }
 }
